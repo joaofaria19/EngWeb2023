a43af04ba0e6d2e550cb0725088a792abb97f159
joaofaria19 <joaofaria.dias04@gmail.com>
2023-02-26 22:26:58 +0000

    servidor feito, e scripts juntos

--- TPC2/scripthtml.py
+++ TPC2/scripthtml.py
@@ -1,12 +1,15 @@
 import json
 
+def ordCidade(cidade):
+    return cidade['nome']
+
 f = open("mapa.json")
 mapa = json.load(f)
-
 
 cidades = mapa['cidades']
 ligacoes = mapa['ligações']
 
+dictDistritos = dict() 
 dictCidades = dict()
 dictIds = dict()
 
@@ -19,8 +22,54 @@ for cidade in cidades:
             lista.append({
                 "id" : local['destino'],
                 "distância" : local['distância']})
+            
+    if cidade['distrito'] not in dictDistritos:
+        dictDistritos[cidade['distrito']] = [cidade]
+    else:
+        cidadesDistrito = dictDistritos[cidade['distrito']]
+        cidadesDistrito.append(cidade)
+        cidadesDistrito.sort(key=ordCidade)
 
+distritos = list(dictDistritos.keys())
+distritos.sort()
 
+# Função para criar a página html do indice
+def cria_indice():
+    indiceHTML ="""<!DOCTYPE html>
+    <html>
+        <head>
+            <title>MapaVirtual</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1>Índice</h1>
+            <ol>
+    """    
+    for d in distritos:
+        indiceHTML+=f"""
+            <li>
+                <h3>{d}</h3>
+                <ul>
+        """
+        lista_cidades = dictDistritos[d]
+        for lc in lista_cidades:
+            indiceHTML+=f"""
+                <li>
+                    <a href='/{lc['id']}'>{lc['nome']}</a>
+                </li>"""
+        indiceHTML+="""
+            </ul>
+        </li>"""
+
+    indiceHTML+="""    
+        </body>
+    </html>
+    """
+    # Salvar a página html com o nome index
+    with open("./html/index.html", "w") as arquivo:
+        arquivo.write(indiceHTML)
+
+# Função para criar a página html de uma cidade especifica
 def pagina_cidade(cidade):
     idcidade = cidade['id']
     nome = cidade['nome']
@@ -32,14 +81,15 @@ def pagina_cidade(cidade):
         <html>
             <head>
                 <title>{nome}</title>
+                <meta charset="utf-8"/>
             </head>
             <body>
                 <a name="{idcidade}"/>
                 <h1>{nome}</h1>
-                <p><h3>População: </h3>{populacao}</p>
-                <p><h3>Descrição: </h3>{descricao}</p>
-                <p><h3>Distrito: </h3>{distrito}</p>
-                <h3>Ligações: </h3>
+                <p><b>População: </b>{populacao}</p>
+                <p><b>Descrição: </b>{descricao}</p>
+                <p><b>Distrito: </b>{distrito}</p>
+                <p><b>Ligações: </b></p>
                 <ul>
     """
     for vizinho in dictIds[idcidade]:
@@ -51,9 +101,11 @@ def pagina_cidade(cidade):
         </html>
     """
     # Salvar a página HTML com o nome da cidade
-    with open(f"{idcidade}.html", "w") as arquivo:
+    with open(f"./html/{idcidade}.html", "w") as arquivo:
         arquivo.write(pageHTML)
 
+# Chamar a função para criar a página do indice
+cria_indice()
 # Chamar a função para cada cidade na lista
 for cidade in cidades:
     pagina_cidade(cidade)
--- TPC2/servidor.js
+++ TPC2/servidor.js
@@ -4,23 +4,25 @@ var fs = require('fs');
 
 
 http.createServer(function(req,res){
-    var pedido = url.parse(req.url,true).pathname.substring(1)
+    var pedido = url.parse(req.url,true).pathname.substring(1);
 
     var d = new Date().toISOString().substring(0,16);
     console.log(req.method+" "+req.url+" "+d);
     if(!pedido){
-        pedido='index'
+        pedido='index';
     }
-    
-    fs.readFile(pedido+'.html', function (err,data) {
+
+    console.log(pedido)
+    fs.readFile("./html/"+pedido+'.html', function (err,data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         if(err){
-            res.write('Erro na leitura do ficheiro' + err)
+            res.write('Erro na leitura do ficheiro ' + err);
+        }else{
+            res.write(data);
         }
-        res.write(data)
         res.end();
 
     })
-}).listen(7778);
+}).listen(7777);
 
-console.log("Servidor à escuta na porta 7778...")
+console.log("Servidor à escuta na porta 7777...")
